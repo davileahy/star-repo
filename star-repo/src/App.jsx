da84f1d55c27ec0107ebc5aa974bd1b49d41d246
davileahy <daivleahy2@gmail.com>
2025-08-08 08:28:34 -0300

    Initial Commit

---
+++ star-repo/src/App.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<h1>Hello World</h1>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
